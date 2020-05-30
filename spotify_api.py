da51d1596f36c2405f54215709c569f308ed639e
William <wl433@cornell.edu>
2020-05-30 13:51:51 -0400

    added search functionality

--- spotify_api.py
+++ spotify_api.py
@@ -1,6 +1,7 @@
 import requests
 import base64
 import datetime
+from urllib.parse import urlencode
 
 client_id = "480b5435dcf240fdbfb3fa533d5ab00d"
 client_secret = "cb1105402e3142b5a52c38f6d44284e8"
@@ -44,7 +45,7 @@ class SpotifyAPI(object):
         token_headers = self.get_token_headers()
         r = requests.post(token_url, data=token_data, headers=token_headers)
         if r.status_code not in range(200, 299):
-            return False
+            raise Exception("Could not authenticate client")
         data = r.json()
         now = datetime.datetime.now()
         access_token = data["access_token"]
@@ -55,7 +56,31 @@ class SpotifyAPI(object):
         self.access_token_did_expire = expires < now
         return True
 
+    def get_access_token(self):
+        token = self.access_token
+        expires = self.access_token_expires
+        now = datetime.datetime.now()
+        if expires < now:
+            self.perform_auth()
+            return self.get_access_token()
+        elif token == None:
+            self.perform_auth()
+            return self.get_access_token()
+        return token
+
+    def search(self, query, search_type='artist'):
+        access_token = self.get_access_token()
+        headers = {"Authorization": f"Bearer {access_token}"}
+        endpoint = "https://api.spotify.com/v1/search"
+        data = urlencode({"q": query, "type": search_type.lower()})
+        lookup_url = f"{endpoint}?{data}"
+        print(lookup_url)
+        r = requests.get(lookup_url, headers=headers)
+        print(r.status_code)
+        if r.status_code not in range(200, 299):
+            return {}
+        return r.json()
+
 
 spotify = SpotifyAPI(client_id, client_secret)
-spotify.perform_auth()
-spotify.access_token
+print(spotify.search("Time", search_type="Track"))
