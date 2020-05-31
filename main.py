c403a2d3c40a561debc66087c50af3f7322c9578
William <wl433@cornell.edu>
2020-05-31 15:22:04 -0400

    basic functionality of backend completed

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ CLIENT_SECRET = "cb1105402e3142b5a52c38f6d44284e8"
 
 
 def main():
+
+    # Begin HTML Scraping
+
     ask_query = input("Lyrics: ")
     soup = create_soup(search_web(ten_urls(ask_query)))
     song_title = find_title(soup)
@@ -15,14 +18,18 @@ def main():
     print("The artist is: " + song_artist)
     print("------Done Scraping------")
 
-    query = {"track": song_title, "artist": song_artist}
+    # Begin Spotify Querying
 
+    query = {"track": song_title, "artist": song_artist}
     spotify = SpotifyAPI(CLIENT_ID, CLIENT_SECRET)
-    print(spotify.search(query, search_type="Track"))
+    json_query = spotify.search(query, search_type="Track")
+    get_track_url = json_query.get("tracks").get(
+        "items")[0].get("external_urls").get("spotify")
+    play_track_preview = json_query.get("tracks").get(
+        "items")[0].get("preview_url")
+
+    return get_track_url
 
 
 if __name__ == '__main__':
     main()
-
-# tracks, items[0], external_urls, spotify-> to open up the link to song
-# tracks, items[0], preview_url -> play the song's sample
